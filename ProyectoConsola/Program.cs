09cfa5360ec1d3aa4201d68a208d9ba3ee49b248
cbdominguez <dominguezrodrigo897@gmail.com>
2026-03-17 11:19:13 -0300

    Cambio en rama cbdominguez

--- ProyectoConsola/Program.cs
+++ ProyectoConsola/Program.cs
@@ -1,1 +1,4 @@
 ﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Cambio en rama cbdominguez");
+Console.WriteLine("Cambio en rama cbdominguez 2");
+Console.WriteLine("Estoy en la rama cbdominguez");
